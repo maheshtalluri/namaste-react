033352606f10368db19c065a054f059104a5f57a
Mahesh Kumar Talluri <tallurimahesh169@gmail.com>
2025-12-07 12:23:37 +0530

    updated on 7-12-2025

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "h1 tag"),
     React.createElement("h2", {}, "h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h1", {}, "h1 tag Mahesh"),
     React.createElement("h2", {}, "h2 tag"),
   ]),
 ]);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React JS!"
-// );
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ README.md
@@ -0,0 +1,1 @@
+![alt text](<parcel apllications in Dev .jpg>)
